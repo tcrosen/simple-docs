28dbc7d2661261a19fe75f29a00035f0b2d99c11
Terry Rosen <tcrosen@gmail.com>
2014-11-20 17:10:25 -0500

    add glob tests

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 'use strict';
 
+var async = require('async');
 var parse = require('comment-parser');
 var fs = require('fs-extra');
 var path = require('path');
+var glob = require('glob');
 
+function writeJson(file, json, done) {
+  fs.outputJson(file, json, function(err) {
+    if (err) {
+      done(err);
+    }
+
+    done(null, json);
+  });
+}
+
+/**
+* Generate docs from a single file
+*/
 function parseFile(file, out, done) {
   var outputFile = path.join(out, 'docs.json');
 
@@ -12,7 +27,7 @@ function parseFile(file, out, done) {
       done(err);
     }
 
-    fs.outputJson(outputFile, parsed, function(err) {
+    writeJson(outputFile, parsed, function(err) {
       if (err) {
         done(err);
       }
@@ -22,6 +37,37 @@ function parseFile(file, out, done) {
   });
 }
 
+/**
+* Recursively generate docs for a directory
+*/
+function parseDir(dir, out, done) {
+  var docs = [];
+
+  glob(path.join(dir, '**/*.js'), function(err, files) {
+    if (err) {
+      done(err);
+    }
+
+    async.each(files, function(file, cb) {
+      parse.file(file, function(err, contents) {
+        if (err) {
+          done(err);
+        }
+
+        docs.push(contents);
+        cb();
+      });
+    }, function(err) {
+      if (err) {
+        done(err);
+      }
+
+      done(null, docs);
+    });
+  });
+}
+
 module.exports = {
-  parseFile: parseFile
+  parseFile: parseFile,
+  parseDir: parseDir
 };
