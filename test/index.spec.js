65c1486ce81bdaf5f5e6f44ad4baada5472f66a8
Terry Rosen <tcrosen@gmail.com>
2014-11-20 15:14:34 -0500

    remove node_modules

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,18 @@
 var expect = require('chai').expect;
 var simpleDocs = require('../index');
+var path = require('path');
+
 var outDir = __dirname + '/out';
 var fixtures = __dirname + '/fixtures';
 
 /* jshint -W030 */
 
 describe('Simple Docs', function() {
+
+  beforeEach(function(done) {
+    // clean output dir
+
+  });
 
   it('should parse a file', function(done) {
     simpleDocs.parseFile(fixtures + '/basic.js', outDir, function(err) {
@@ -14,4 +21,12 @@ describe('Simple Docs', function() {
     });
   });
 
+  it('should output JSON', function(done) {
+    simpleDocs.parseFile(fixtures + '/basic.js', outDir, function(err) {
+      fs.readFile(path.join(outDir, 'docs.json'), function(err, contents) {
+        expect('')
+        done();
+      });
+    });
+  });
 });
